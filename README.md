c2800e121cc820c566d371a9589df821eb533be6
Skaya Mokma <skaya.mokma@icloud.com>
2023-11-16 19:57:36 -0500

    Adding state/country code feature

--- README.md
+++ README.md
@@ -1,11 +1,14 @@
 # Skycast
-Skycast is a weather app where users can search US cities to see the current temperature and weather conditions there.
+Skycast is a weather app where users can search the current temperature and weather conditions of any city in the world.
 
 Live Demo: https://skaya.z13.web.core.windows.net/
 
 # API 
-Skycast is brought to you by the high performance weather API
+Skycast is brought to you by the high performance weather API, 
+
 Weatherbit: https://www.weatherbit.io/
+
+
 
 
 
--- main.js
+++ main.js
@@ -28,79 +28,86 @@ async function getWeather(){
     let weatherData = await res.json()
     console.log(weatherData)
     hiddenResponse.removeAttribute("hidden")
-    resultCityName.innerHTML = `${weatherData.data[0].city_name}, ${weatherData.data[0].state_code}`
+    if (`${weatherData.data[0].state_code}` === 'DC'){
+        resultCityName.innerHTML = `${weatherData.data[0].city_name}`
+    } else if (isNaN(parseInt(weatherData.data[0].state_code)) === false){
+        resultCityName.innerHTML = `${weatherData.data[0].city_name}, ${weatherData.data[0].country_code}`
+    } else {
+        resultCityName.innerHTML = `${weatherData.data[0].city_name}, ${weatherData.data[0].state_code} - ${weatherData.data[0].country_code}`
+    }
+    
     resultCityTemp.innerHTML = `${weatherData.data[0].temp} °F`
 
     let resultCityWeatherConditionsData = `${weatherData.data[0].weather.code}`
+    console.log(resultCityWeatherConditionsData)
     
     if (resultCityWeatherConditionsData < 300){
       resultCityWeatherConditionsIcon.src = "https://cdn1.iconfinder.com/data/icons/weather-471/128/THUNDERSTORM-512.png"
     }
-    else if (300 <= resultCityWeatherConditionsData < 600 ){
+    else if (300 <= resultCityWeatherConditionsData && resultCityWeatherConditionsData < 600 ){
       resultCityWeatherConditionsIcon.src = "https://cdn2.iconfinder.com/data/icons/weather-flat-14/64/weather07-1024.png"
     }
-    else if (600 <= resultCityWeatherConditionsData < 700){
+    else if (600 <= resultCityWeatherConditionsData && resultCityWeatherConditionsData < 700){
       resultCityWeatherConditionsIcon.src = "https://cdn3.iconfinder.com/data/icons/sunnyday-2/142/full_cloudy_snow-1024.png"
     } 
-    else if (resultCityWeatherConditionsData == 800){
+    else if (resultCityWeatherConditionsData === 800){
       resultCityWeatherConditionsIcon.src = "https://static.vecteezy.com/system/resources/previews/000/551/126/original/vector-sun-icon.jpg"
     } 
     else if (700 <= resultCityWeatherConditionsData < 800 || 801 <= resultCityWeatherConditionsData <= 900) {
       resultCityWeatherConditionsIcon.src = "https://tse1.mm.bing.net/th?id=OIP.1_PNYSfxg2MHtMSPRvKd8wHaHa&pid=Api&rs=1&c=1&qlt=95&w=117&h=117"
     }
-    console.log(resultCityWeatherConditionsIcon.src)
   }
 
   currentWeatherSearchBar.addEventListener("click", getWeather)
 
-async function displayWeather(){
+// async function displayWeather(){
 
-  const cityOne = document.getElementById("city-one-name")
-  const cityOneTemp = document.getElementById("city-one-temp")
-  const cityTwo = document.getElementById("city-two-name")
-  const cityTwoTemp = document.getElementById("city-two-temp")
-  const cityThree = document.getElementById("city-three-name")
-  const cityThreeTemp = document.getElementById("city-three-temp")
-  const cityFour = document.getElementById("city-four-name")
-  const cityFourTemp = document.getElementById("city-four-temp")
+//   const cityOne = document.getElementById("city-one-name")
+//   const cityOneTemp = document.getElementById("city-one-temp")
+//   const cityTwo = document.getElementById("city-two-name")
+//   const cityTwoTemp = document.getElementById("city-two-temp")
+//   const cityThree = document.getElementById("city-three-name")
+//   const cityThreeTemp = document.getElementById("city-three-temp")
+//   const cityFour = document.getElementById("city-four-name")
+//   const cityFourTemp = document.getElementById("city-four-temp")
 
-  let cityOneUrl = `https://api.weatherbit.io/v2.0/current?key=${accessKey}&city=Raleigh,NC&units=I`
-  const cityOneRes = await fetch(cityOneUrl)
-  let cityOneWeatherData = await cityOneRes.json()
-  console.log(cityOneWeatherData)
+//   let cityOneUrl = `https://api.weatherbit.io/v2.0/current?key=${accessKey}&city=Raleigh,NC&units=I`
+//   const cityOneRes = await fetch(cityOneUrl)
+//   let cityOneWeatherData = await cityOneRes.json()
+//   console.log(cityOneWeatherData)
 
-  cityOne.innerHTML = `${cityOneWeatherData.data[0].city_name}, ${cityOneWeatherData.data[0].state_code}`
-  cityOneTemp.innerHTML = `${cityOneWeatherData.data[0].temp} °F`
+//   cityOne.innerHTML = `${cityOneWeatherData.data[0].city_name}, ${cityOneWeatherData.data[0].state_code}`
+//   cityOneTemp.innerHTML = `${cityOneWeatherData.data[0].temp} °F`
   
 
-  let cityTwoUrl = `https://api.weatherbit.io/v2.0/current?key=${accessKey}&city=Chicago,Il&units=I`
-  const cityTwoRes = await fetch(cityTwoUrl)
-  let cityTwoWeatherData = await cityTwoRes.json()
-  console.log(cityTwoWeatherData)
+//   let cityTwoUrl = `https://api.weatherbit.io/v2.0/current?key=${accessKey}&city=Chicago,Il&units=I`
+//   const cityTwoRes = await fetch(cityTwoUrl)
+//   let cityTwoWeatherData = await cityTwoRes.json()
+//   console.log(cityTwoWeatherData)
 
-  cityTwo.innerHTML = `${cityTwoWeatherData.data[0].city_name}, ${cityTwoWeatherData.data[0].state_code}`
-  cityTwoTemp.innerHTML = `${cityTwoWeatherData.data[0].temp} °F`
+//   cityTwo.innerHTML = `${cityTwoWeatherData.data[0].city_name}, ${cityTwoWeatherData.data[0].state_code}`
+//   cityTwoTemp.innerHTML = `${cityTwoWeatherData.data[0].temp} °F`
 
-  let cityThreeUrl = `https://api.weatherbit.io/v2.0/current?key=${accessKey}&city=Seattle,WA&units=I`
-  const cityThreeRes = await fetch(cityThreeUrl)
-  let cityThreeWeatherData = await cityThreeRes.json()
-  console.log(cityThreeWeatherData)
+//   let cityThreeUrl = `https://api.weatherbit.io/v2.0/current?key=${accessKey}&city=Seattle,WA&units=I`
+//   const cityThreeRes = await fetch(cityThreeUrl)
+//   let cityThreeWeatherData = await cityThreeRes.json()
+//   console.log(cityThreeWeatherData)
 
-  cityThree.innerHTML = `${cityThreeWeatherData.data[0].city_name}, ${cityThreeWeatherData.data[0].state_code}`
-  cityThreeTemp.innerHTML = `${cityThreeWeatherData.data[0].temp} °F`
+//   cityThree.innerHTML = `${cityThreeWeatherData.data[0].city_name}, ${cityThreeWeatherData.data[0].state_code}`
+//   cityThreeTemp.innerHTML = `${cityThreeWeatherData.data[0].temp} °F`
 
-  let cityFourUrl = `https://api.weatherbit.io/v2.0/current?key=${accessKey}&city=Phoenix,AZ&units=I`
-  const cityFourRes = await fetch(cityFourUrl)
-  let cityFourWeatherData = await cityFourRes.json()
-  console.log(cityFourWeatherData)
+//   let cityFourUrl = `https://api.weatherbit.io/v2.0/current?key=${accessKey}&city=Phoenix,AZ&units=I`
+//   const cityFourRes = await fetch(cityFourUrl)
+//   let cityFourWeatherData = await cityFourRes.json()
+//   console.log(cityFourWeatherData)
 
-  cityFour.innerHTML = `${cityFourWeatherData.data[0].city_name}, ${cityFourWeatherData.data[0].state_code}`
-  cityFourTemp.innerHTML = `${cityFourWeatherData.data[0].temp} °F`
+//   cityFour.innerHTML = `${cityFourWeatherData.data[0].city_name}, ${cityFourWeatherData.data[0].state_code}`
+//   cityFourTemp.innerHTML = `${cityFourWeatherData.data[0].temp} °F`
   
-}
+// }
 
 
-displayWeather()
+// displayWeather()
 
 
 function CtoF(){
